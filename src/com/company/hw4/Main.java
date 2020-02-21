85627b902868b1f28f99fe1a91afdae1aced9565
groseal <59777269+groseal@users.noreply.github.com>
2020-02-21 20:46:39 +0300

    новое изменение 8

--- src/com/company/hw4/Main.java
+++ src/com/company/hw4/Main.java
@@ -2,7 +2,7 @@ package com.company.hw4;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 2;
+        int a = 3;
         int b = 4;
         int c = a+b;
         System.out.println(c);
