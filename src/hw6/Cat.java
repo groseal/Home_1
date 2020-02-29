e7c353a7ccd7cdf526673563ef7eca63f24e6d0a
groseal <59777269+groseal@users.noreply.github.com>
2020-02-29 22:44:53 +0300

    новое изменение 10

---
+++ src/hw6/Cat.java
@@ -0,0 +1,39 @@
+package hw6;
+
+public class Cat extends Animals {
+
+
+    public Cat(int run, int sail, double jump) {
+        super(run, sail, jump);
+    }
+
+    @Override
+    public boolean letRun(int run) {
+        if (run <= 200) {
+            System.out.println("run: true");
+            return true;
+        }
+        System.out.println("run: false");
+        return false;
+    }
+
+    @Override
+    public boolean letSail(int sail) {
+        if (sail <= 0) {
+            System.out.println("run: true");
+            return true;
+        }
+        System.out.println("sail: false");
+        return false;
+    }
+
+    @Override
+    public boolean letJump(double jump) {
+        if (jump <= 2) {
+            System.out.println("run: true");
+            return true;
+        }
+        System.out.println("jump: false");
+        return false;
+    }
+}
---
+++ src/hw6/Main.java
@@ -0,0 +1,18 @@
+package hw6;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat musja = new Cat(100, 0, 1.8);
+        Cat adolf = new Cat(201, 3, 2.2);
+        Dog drujok = new Dog(400, 8, 0.3);
+        Dog sharik = new Dog(550, 11, 0.6);
+
+        musja.letRun(100);
+        adolf.letJump(2);
+        drujok.letSail(10);
+        drujok.letRun(50);
+        sharik.letRun(600);
+
+
+    }
+}
