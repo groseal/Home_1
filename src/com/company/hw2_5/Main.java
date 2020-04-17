380f4592b5fdb8bc54937ba5a45001a2f690710e
groseal <59777269+groseal@users.noreply.github.com>
2020-04-18 00:42:56 +0300

    новое изменение 22

--- src/com/company/hw2_5/Main.java
+++ src/com/company/hw2_5/Main.java
@@ -1,5 +1,7 @@
 package com.company.hw2_5;
 
+import java.util.Arrays;
+
 public class Main {
     static final int size = 10000000;
     static final int h = size / 2;
@@ -7,23 +9,29 @@ public class Main {
     public static void main(String[] args) {
         float[] arr = new float[size];
         consistently(arr);
+        float[] arr1 = new float[size];
+        System.arraycopy(arr, 0, arr1, 0, size);
         parallel(arr);
+        System.out.println(Arrays.equals(arr1, arr));
+        System.out.println(arr1[1]+" "+arr1[h-1]+" "+arr1[h]+" "+arr1[h+1]);
+        System.out.println(arr[1]+" "+arr[h-1]+" "+arr[h]+" "+arr[h+1]);
+
     }
 
     static void consistently(float[] arr) {
-        for (float i : arr) {
-            arr[(int) i] = 1;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
         }
         long a = System.currentTimeMillis();
-        for (float i : arr) {
-            arr[(int) i] = (float) (arr[(int) i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println("Время выполнения вычислений по формуле беспоточным методом " + (System.currentTimeMillis() - a) + " миллисекунд.");
     }
 
     static void parallel(float[] arr) {
-        for (float i : arr) {
-            arr[(int) i] = 1;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
         }
         float[] arr1 = new float[h];
         float[] arr2 = new float[h];
@@ -31,14 +39,14 @@ public class Main {
         System.arraycopy(arr, h, arr2, 0, h);
 
         Thread newThreadArr1 = new Thread(() -> {
-            for (float i : arr1) {
-                arr1[(int) i] = (float) (arr1[(int) i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            for (int i =0; i < arr1.length; i++) {
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
 
         Thread newThreadArr2 = new Thread(() -> {
-            for (float i : arr2) {
-                arr2[(int) i] = (float) (arr2[(int) i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            for (int i = h; i < arr2.length; i++) {
+                arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
 
@@ -58,7 +66,7 @@ public class Main {
         }
 
         System.arraycopy(arr1, 0, arr, 0, h);
-        System.arraycopy(arr2, 0, arr, h, h);
+        System.arraycopy(arr2, 0, arr, h, arr2.length);
         System.out.println("Время выполнения вычислений по формуле поточным методом " + (System.currentTimeMillis() - a) + " миллисекунд.");
     }
 }
